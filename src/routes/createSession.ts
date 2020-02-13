cb2cc9671519855efc54925caa878b3ff277d7cf
nmaswood98 <30912532+nmaswood98@users.noreply.github.com>
2020-02-13 15:18:38 -0500

    SMS-46 Added datamodels for session, sessionUser and admin, added user collection creation with admin added when createSession is called

--- src/routes/createSession.ts
+++ src/routes/createSession.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import { SessionDataModel } from './models/sessionData.model';
+import { SessionUserDataModel } from './models/sessionUserData.model';
 const fbAdmin = require('firebase-admin');
 const db = fbAdmin.firestore();
 
@@ -12,11 +14,25 @@ class CreateSessionController {
   }
  
   get = (request, response) => {
-    db.collection("Sessions").add({
-      "dateCreated": fbAdmin.firestore.FieldValue.serverTimestamp(),
-      "type": "private"
-    }).then((doc)=>{
-      response.send(`Session Created Session ID is ${doc.id}`);
+
+
+    let sessionData: SessionDataModel = {
+      dateCreated: fbAdmin.firestore.FieldValue.serverTimestamp(),
+      type: "private",
+      startingBalance: 10000
+    }
+
+    let sessionUserData: SessionUserDataModel = {
+      id:"PLACEHOLDER ID",
+      liquid: 10000,
+      type: "Admin"
+    }
+
+    // SHOULD change this to a firestore transactions so its atomic
+    db.collection("Sessions").add(sessionData).then((doc)=>{
+      db.collection("Sessions").doc(doc.id).collection("Users").add(sessionUserData).then(()=>{
+        response.send(doc.id);
+      });
     }).catch((err)=>{
       console.log(err);
       response.send("Could not create session");
---
+++ src/routes/models/stockData.model.ts
@@ -0,0 +1,6 @@
+export interface StockDataModel {
+    symbol?: string;
+    name: string;
+    price: Number;
+    sector: string;
+}
